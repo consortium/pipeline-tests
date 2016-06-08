f445d69d3b8fcc22db31c7a66c30476cc5c9efad
Eric Schrijver <eric@ericschrijver.nl>
2016-06-08 19:23:42 +0200

    Collate all word DOCs into one, generate with custom pandoc template
    Pandoc is able to detect the title because we use the Title paragraph style
    
    We use some basic scholarly HTML and this fixes some of the warning
    cf http://scholarlyhtml.org/core-specification/

--- make-html.sh
+++ make-html.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
 
-# convert various docx files to one html file using pandoc
-
-[ -f reproducing-autonomy.html ] && rm reproducing-autonomy.html
-
-touch reproducing-autonomy.html
-
-# write html header
-echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <meta http-equiv="Content-Style-Type" content="text/css" />
-  <meta name="generator" content="pandoc" />
-  <title></title>
-  <style type="text/css">code{white-space: pre;}</style>
-  <link href="Styles/internal-1.css" rel="stylesheet" type="text/css"/>
-</head>
-<body>
-' >> reproducing-autonomy.html
-
-# write html fragment for each *.docx
-for f in text/0*
-do
-  echo "Processing $f file.."
-  pandoc $f >> reproducing-autonomy.html
-done
-
-# write footer
-echo '</body>
-</html>
-' >> reproducing-autonomy.html
+pandoc --template template.html -f docx -t html text/reproducing-autonomy.docx > reproducing-autonomy.html
 
 # This makes the package needed for epub2html
 # add folders: Fonts Styles
-zip -r reproducing-autonomy.zip reproducing-autonomy.html Fonts Styles -x "*/\.DS_Store" "/\.*"
+zip -r reproducing-autonomy.zip reproducing-autonomy.html Fonts Styles Images -x "*/\.DS_Store" "/\.*"
+
+CURRENT_FOLDER=`pwd`
+
+cd /Volumes/Switzerland/e/Projects/html2epub
+make conversion IN_FILE=/Volumes/Switzerland/e/Projects/pipeline-tests/reproducing-autonomy.zip
+mkdir -p output/reproducing-autonomy.zip/check-epub
+cp -p output/reproducing-autonomy.zip/reproducing-autonomy.epub output/reproducing-autonomy.zip/check-epub/
+unzip output/reproducing-autonomy.zip/check-epub/reproducing-autonomy.epub -d output/reproducing-autonomy.zip/check-epub
+
+
+cd $CURRENT_FOLDER
