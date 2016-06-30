d2fe19850d2545379529e668f8a1c4deae573591
Eric Schrijver <eric@ericschrijver.nl>
2016-06-30 16:42:03 +0200

    Several touchups
    (were done earlier this month)

--- touchups.py
+++ touchups.py
@@ -4,5 +4,7 @@ t = open("reproducing-autonomy.html", "r", "utf-8").read()
 
 t = t.replace('<p>by Kerstin Stakemeier &amp; Marina Vishmidt</p>','<p class="author" property="http://purl.org/dc/terms/creator">by Kerstin Stakemeier &amp; Marina Vishmidt</p>')
 
+t = t.replace('<p>Mute Books, LondonMUTE LOGO</p>','<div class="white">&nbsp;</div>\n<p><img width="123" height="39" src="Images/Mute-logo2.png"/></p>')
+
 o = open("reproducing-autonomy.html", "w", "utf-8")
 o.write(t)
